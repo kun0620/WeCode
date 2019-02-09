3c0e886f2d3eac6b9d420bf8f7367c51e53b396e
kun0620 <41094738+kun0620@users.noreply.github.com>
2019-02-09 10:37:42 +0700

    step two

---
+++ src/app/app.component.ts
@@ -0,0 +1,60 @@
+import { Component } from '@angular/core';
+
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html'
+})
+export class AppComponent {
+  public appPages = [
+    {
+      title: 'Home',
+      url: '/home',
+      icon: 'home'
+    },
+    {
+      title: 'List',
+      url: '/list',
+      icon: 'list'
+    },
+    {
+      title: 'HTML',
+      url: '/html',
+      icon: 'logo-html5'
+    },
+    {
+      title: 'CSS',
+      url: '/css',
+      icon: 'logo-css3'
+    },
+    {
+      title: 'SQL',
+      url: '/sql',
+      icon: 'folder'
+    }
+    ,
+    {
+      title: 'PHP',
+      url: '/php',
+      icon: 'stats'
+    }
+  ];
+
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+}
