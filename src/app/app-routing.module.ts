f488ccdb14e79d0360310d22f8159549f644c3d9
kun0620 <41094738+kun0620@users.noreply.github.com>
2019-02-10 14:49:39 +0700

    add page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,8 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: './list/list.module#ListPageModule'
-  }
+  },
+  { path: 'html', loadChildren: './html/html.module#HtmlPageModule' }
   
 ];
 
---
+++ src/app/html/html.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { HtmlPage } from './html.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HtmlPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [HtmlPage]
+})
+export class HtmlPageModule {}
